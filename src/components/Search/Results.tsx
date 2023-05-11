ca476d08e3843d641309e6f706e62f273df25521
Elisei.Nicolae <nicolae.elisei1999@gmail.com>
2023-05-12 00:41:25 +0300

    feat: update <Result /> component -  (progress 2)

--- src/components/Search/Results.tsx
+++ src/components/Search/Results.tsx
@@ -1,29 +1,42 @@
 import {Box, Image} from "@chakra-ui/react";
 
 export default function Results({data}: any) {
-  console.log("data", data)
   return (
     <div>
       <h1>Results</h1>
-      {data?.collection?.items
-        .map((item: any, index: number) => {
-          return (
-            <>
-              {
-                item?.links?.map((link: any, index: number) => {
-                  if (link?.render === "image") {
+      <Box display={"flex"} gap={50} flexDirection={"column"}>
+        {data?.collection?.items
+          .map((item: any, index: number) => {
+            return (
+              <Box key={`image-container-${index}`} maxW={500} display={"flex"} justifyContent={"center"}
+                   alignItems={"center"} flexDirection={"column"}>
+                {
+                  item?.links?.map((link: any, index: number) => {
+                    if (link?.render === "image") {
+                      return (
+                        <Box key={`image-${index}`} display={"flex"} justifyContent={"center"} alignItems={"center"} height={250} width={350}>
+                          <Image src={link?.href} alt={"123"} loading={"lazy"} objectFit={"contain"}
+                                 width={'100%'} height={"100%"}/>
+                        </Box>
+                      )
+                    }
+                  })
+                }
+                {
+                  item?.data?.map((data: any, index: number) => {
                     return (
-                      <Box display={"flex"} justifyContent={"center"} alignItems={"center"} height={350} width={350}>
-                        <Image key={index} src={link?.href} alt={"123"} loading={"lazy"} objectFit={"contain"}
-                               width={'100%'} height={"100%"}/>
-                      </Box>
+                      <div key={`description-${index}`}>
+                        <h1>{data?.title}</h1>
+                        <h1>{data?.location}</h1>
+                        <h1>{data?.photographer}</h1>
+                      </div>
                     )
-                  }
-                })
-              }
-            </>
-          )
-        })}
+                  })
+                }
+              </Box>
+            )
+          })}
+      </Box>
     </div>
   )
 }
